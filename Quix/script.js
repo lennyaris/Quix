9da82cff3a578902d919c1a63f9766fdb76db117
Bautista Aris <130247379+lennyaris@users.noreply.github.com>
2023-08-05 21:46:18 -0300

    NAV/INICIO/BENEFICIOS/PASOS/NOSOTROS

--- Quix/script.js
+++ Quix/script.js
@@ -1,3 +1,4 @@
+// JavaScript
 let scrolled = false;
 let previousScrollY = 0;
 
@@ -9,31 +10,66 @@ function onScroll() {
     scrolled = true;
   }
 
-  const sections = document.querySelectorAll('.section');
   const currentScrollY = window.scrollY;
   const direction = currentScrollY > previousScrollY ? 'down' : 'up';
+  const sweepElements = document.querySelectorAll('.sweep');
 
-  if (currentScrollY !== previousScrollY) {
-    sections.forEach((section) => {
-      if (direction === 'down') {
-        const nextSection = section.nextElementSibling;
-        if (nextSection) {
-          nextSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-          return;
-        }
-      } else if (direction === 'up') {
-        const prevSection = section.previousElementSibling;
-        if (prevSection) {
-          prevSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-          return;
+  sweepElements.forEach((element) => {
+    if (!element.classList.contains('shown')) {
+      const rect = element.getBoundingClientRect();
+      const isVisible = rect.top < window.innerHeight;
+
+      if (isVisible) {
+        element.classList.add('shown');
+      }
+    }
+  });
+
+  const steps = document.querySelectorAll('.step');
+  steps.forEach((step, index) => {
+    if (!step.classList.contains('shown')) {
+      const rect = step.getBoundingClientRect();
+      const isVisible = rect.left < window.innerWidth;
+
+      if (isVisible) {
+        step.classList.add('shown');
+        // Ajusta la velocidad de cada paso
+        step.style.animationDelay = `${index * 0.5}s`;
+
+        // Cargar la imagen utilizando Intersection Observer
+        const img = step.querySelector('img.lazy');
+        if (img) {
+          const observer = new IntersectionObserver((entries, observer) => {
+            entries.forEach((entry) => {
+              if (entry.isIntersecting) {
+                img.src = img.dataset.src;
+                img.classList.remove('lazy');
+                observer.unobserve(img);
+              }
+            });
+          }, { threshold: 0.5 });
+
+          observer.observe(img);
         }
       }
-    });
+    }
+  });
 
-    previousScrollY = currentScrollY;
-  }
+  previousScrollY = currentScrollY;
 }
 
 window.addEventListener('scroll', onScroll);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
