d9b888a1766fc243b7d78953de55150c85d98e15
Bautista Aris <130247379+lennyaris@users.noreply.github.com>
2023-08-10 20:14:10 -0300

    FOOTER / NAV E INICIO (responsive)

--- Quix/script.js
+++ Quix/script.js
@@ -35,22 +35,6 @@ function onScroll() {
         step.classList.add('shown');
 
         step.style.animationDelay = `${index * 0.5}s`;
-
-
-        const img = step.querySelector('img.lazy');
-        if (img) {
-          const observer = new IntersectionObserver((entries, observer) => {
-            entries.forEach((entry) => {
-              if (entry.isIntersecting) {
-                img.src = img.dataset.src;
-                img.classList.remove('lazy');
-                observer.unobserve(img);
-              }
-            });
-          }, { threshold: 0.5 });
-
-          observer.observe(img);
-        }
       }
     }
   });
@@ -60,9 +44,31 @@ function onScroll() {
 
 window.addEventListener('scroll', onScroll);
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
 function redirectToPage() {
   window.location.href = "contacto.html";
 }
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navList = document.querySelector('.nav-list');
+
+menuToggle.addEventListener('click', () => {
+  navList.classList.toggle('nav-list-visible');
+});
+
+
+
+
+
 
 
 
